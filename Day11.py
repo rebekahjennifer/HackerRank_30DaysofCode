564cc626a82cd90d3badd7fbb8ca0dacf5936d16
rebekahjennifer <rebekahjennifer02@gmail.com>
2020-05-23 01:29:25 +0100

    2D Arrays

---
+++ Day11.py
@@ -0,0 +1,26 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+        
+    hourglass = []
+    
+
+    for x in range(0, 4):
+        for y in range(0, 4):
+            s = sum(arr[x][y:y+3]) + arr[x+1][y+1] + sum(arr[x+2][y:y+3])
+            hourglass.append(s)
+            hg = max(hourglass) 
+            
+
+    print (hg)
+#Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
